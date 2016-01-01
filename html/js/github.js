a33f2bb20342cc58503343873f1488aefd5b28ba
TheConnMan <bcconn2112@gmail.com>
2016-01-01 17:34:10 -0500

    Add repo list rendering to the landing page

---
+++ html/js/github.js
@@ -0,0 +1,38 @@
+var repos = [{
+	owner: 'theconnman',
+	repo: 'Transient',
+	port: 8080
+}];
+
+function renderList() {
+	Q.all(repos.map(getRepo)).then(function(repos) {
+		$('.ui.list').html(repos.map(renderRepo).join(''));
+	});
+}
+
+function getRepo(repoInfo) {
+	var d = Q.defer();
+	$.ajax({
+		url: 'https://api.github.com/repos/' + repoInfo.owner + '/' + repoInfo.repo,
+		success: function(data) {
+			data.port = repoInfo.port;
+			d.resolve(data);
+		}
+	});
+	return d.promise;
+}
+
+function renderRepo(repo) {
+	return [
+		'<div class="item">',
+			'<img class="ui avatar image" src="' + repo.owner.avatar_url + '">',
+			'<div class="content">',
+				'<a href="#" onclick="window.location.port=' + repo.port + '" class="header">' + repo.name + '</a>',
+				'<div class="description">',
+					repo.description,
+					' (<a href="' + repo.html_url + '">GitHub</a>)',
+				'</div>',
+			'</div>',
+		'</div>'
+	].join('');
+}
